28c3c8b3447800d7e1cd9496b3f0ebd3d9505b05
snehalNarkar <snehal.narkar@sweetli.me>
2019-04-10 13:25:30 +0530

    update server.js file

--- server.js
+++ server.js
@@ -1,55 +1,63 @@
-const http = require("http");
-const fs = require("fs");
+// server.js
+// where your node app starts
+
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+const http = require('http');
+const fs = require('fs');
 
 const getDate = date => ({
   unix: date.getTime(),
   utc: date.toUTCString()
 });
 
-const requestHandler = (req, res) => {
-  if (req.url === "/") {
-    fs.readFile("views/index.html", (err, html) => {
-      if (err) throw err;
-
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(html);
-    });
-  } else if (req.url.startsWith("/api/timestamp")) {
-    const dateString = req.url.split("/api/timestamp/")[1];
-    let timestamp;
-
-    if (dateString === undefined || dateString.trim() === "") {
-      timestamp = getDate(new Date());
-    } else {
-      const date = !isNaN(dateString)
-        ? new Date(parseInt(dateString))
-        : new Date(dateString);
-
-      if (!isNaN(date.getTime())) {
-        timestamp = getDate(date);
-      } else {
-        timestamp = {
-          error: "Invalid Date"
-        };
-      }
-    }
-
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(timestamp));
-  } else {
-    fs.readFile("views/error.html", (err, html) => {
-      if (err) throw err;
-
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end(html);
-    });
+app.get('/api/timestamp/', function(req, res){
+  const dateString = req.url.split("/api/timestamp/")[1];
+  let timestamp;
+  if(dateString === undefined || dateString.trim === ""){
+    timestamp = getDate(new Date());
   }
-};
+  else{
+    const date = !isNaN(dateString) ? new Date(parseInt(dateString)) : new Date(dateString);
+    if(!NaN(date.getTime())){
+      timestamp = getDate(date);
+    }
+    else{
+      timestamp = { error: 'Invalid Date' }
+    };
+  }
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(timestamp));
+});
 
-const server = http.createServer(requestHandler);
 
-server.listen(process.env.PORT || 4100, err => {
-  if (err) throw err;
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require('cors');
+app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-  console.log(`Server running on PORT ${server.address().port}`);
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({greeting: 'hello API'});
+});
+
+
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
 });
